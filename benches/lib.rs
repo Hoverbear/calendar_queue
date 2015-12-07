4eeec12c67228565292a841f8c1385e582cac7e2
Andrew Hobden <andrew@hoverbear.org>
2015-12-07 11:44:12 -0800

    Make benchmarks itemized

--- benches/lib.rs
+++ benches/lib.rs
@@ -8,8 +8,8 @@ use rand::distributions::{IndependentSample, Range};
 use calendar_queue::{CalendarQueue};
 use test::Bencher;
 
-const QUEUE_SIZE: u64 = 100_000;
 const MANY_CHANNELS: u64 = 1_000;
+const SOME_CHANNELS: u64 = MANY_CHANNELS / 2;
 
 #[bench]
 fn single_channel(b: &mut Bencher) {
@@ -20,11 +20,9 @@ fn single_channel(b: &mut Bencher) {
         .unwrap();
 
     b.iter(|| {
-        for _ in 0..QUEUE_SIZE {
-            assert_eq!(queue.tick(), Some("Foo".into()));
-            sender.send("Foo".into())
-                .unwrap();
-        }
+        assert_eq!(queue.tick(), Some("Foo".into()));
+        sender.send("Foo".into())
+            .unwrap();
     });
 }
 
@@ -41,16 +39,14 @@ fn alternating_dual_channel(b: &mut Bencher) {
         .unwrap();
 
     b.iter(|| {
-        for _ in 0..QUEUE_SIZE {
-            assert_eq!(queue.tick(), Some("Foo".into()));
-            // Replenish it.
-            sender_1.send("Foo".into())
-                .unwrap();
-            assert_eq!(queue.tick(), Some("Bar".into()));
-            // Replenish it.
-            sender_2.send("Bar".into())
-                .unwrap();
-        }
+        assert_eq!(queue.tick(), Some("Foo".into()));
+        // Replenish it.
+        sender_1.send("Foo".into())
+            .unwrap();
+        assert_eq!(queue.tick(), Some("Bar".into()));
+        // Replenish it.
+        sender_2.send("Bar".into())
+            .unwrap();
     });
 }
 
@@ -70,9 +66,28 @@ fn many_channels(b: &mut Bencher) {
 
 
     b.iter(|| {
-        for _ in 0..(QUEUE_SIZE) {
-            let i = queue.next().unwrap();
-            channels[i as usize].send(i).unwrap()
-        }
+        let i = queue.next().unwrap();
+        channels[i as usize].send(i).unwrap()
+    });
+}
+
+#[bench]
+fn some_channels(b: &mut Bencher) {
+    let mut queue = CalendarQueue::new();
+
+    let between = Range::new(10, 500);
+    let mut rng = rand::thread_rng();
+
+    let channels = (0..SOME_CHANNELS).map(|i| {
+        let choice = between.ind_sample(&mut rng);
+        let sender = queue.create_channel(i, choice).unwrap();
+        sender.send(i).unwrap();
+        sender
+    }).collect::<Vec<_>>();
+
+
+    b.iter(|| {
+        let i = queue.next().unwrap();
+        channels[i as usize].send(i).unwrap()
     });
 }
